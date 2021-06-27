f0ac4c4bfbad63be42c29417b9549e11ed1dc444
Chu Minh <dotranminhchu@gmail.com>
2021-06-27 23:46:07 +0700

    Change struct

--- IOHelper.java
+++ IOHelper.java
@@ -1,12 +1,12 @@
 package Helper;
 
 public class IOHelper {
-    public static int IsnputInt(String mess){
+    public static int IsnputInt(String msg){
         java.util.Scanner scanner = new java.util.Scanner(System.in);
+        int value;
         do{
-            int value;
             try{
-                System.out.print(mess);value=Integer.parseInt(scanner.nextLine());
+                System.out.print(msg);value=Integer.parseInt(scanner.nextLine());
                 scanner.close();
                 return value;
             }catch (Exception e){
@@ -14,12 +14,12 @@ public class IOHelper {
             }
         }while (true);
     }
-    public static float InputFloat(String mess){
+    public static float InputFloat(String msg){
         java.util.Scanner scanner = new java.util.Scanner(System.in);
+        float value;
         do{
-            float value;
             try{
-                System.out.print(mess);value=Float.parseFloat(scanner.nextLine());
+                System.out.print(msg);value=Float.parseFloat(scanner.nextLine());
                 scanner.close();
                 return value;
             }catch (Exception e){
@@ -27,12 +27,25 @@ public class IOHelper {
             }
         }while (true);
     }
-    public static double InputDouble(String mess){
+    public static double InputDouble(String msg){
+        double value;
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
         do{
-            double value;
             try{
-                java.util.Scanner scanner = new java.util.Scanner(System.in);
-                System.out.print(mess);value=Double.parseDouble(scanner.nextLine());
+                System.out.print(msg);value=Double.parseDouble(scanner.nextLine());
+                scanner.close();
+                return value;
+            }catch (Exception e){
+                System.out.println("[!] Re-enter real number please [!]");
+            }
+        }while (true);
+    }
+    public static long InputLong(String msg){
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        long value;
+        do{
+            try{
+                System.out.print(msg);value=Long.parseLong(scanner.nextLine());
                 scanner.close();
                 return value;
             }catch (Exception e){
@@ -41,26 +54,26 @@ public class IOHelper {
         }while (true);
     }
 
-    public static String InputString(String mess){
+    public static String InputString(String msg){
         String value;
         java.util.Scanner scanner = new java.util.Scanner(System.in);
-        System.out.print(mess);value=scanner.nextLine();
+        System.out.print(msg);value=scanner.nextLine();
         scanner.close();
         return value;
     }
-    public static String InputString(String mess,boolean trim){
+    public static String InputString(String msg,boolean trim){
         String value;
         java.util.Scanner scanner = new java.util.Scanner(System.in);
-        System.out.print(mess);value=scanner.nextLine();
+        System.out.print(msg);value=scanner.nextLine();
         scanner.close();
         if(trim)return value.trim();
         else return value;
     }
-    public static String InputPattern(String mess,String pattern){
+    public static String InputPattern(String msg,String pattern){
         String value;
         java.util.Scanner scanner = new java.util.Scanner(System.in);
         do{
-            System.out.print(mess);value=scanner.nextLine();
+            System.out.print(msg);value=scanner.nextLine();
         }while (!value.matches(pattern));
 
         scanner.close();
